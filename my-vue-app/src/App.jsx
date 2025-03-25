4acdf0dd853cf8ba84d3bac6b1dfb226702e4600
Ahsaan-ullah3236 <158459990+Ahsaan-ullah3236@users.noreply.github.com>
2025-03-25 12:47:46 +0500

    Initial commit with Vue and React projects

---
+++ my-vue-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-xl font-bold text-orange-500 bg-black">
+        This is my first project task
+      </h1>
+    </>
+  )
+}
+
+export default App
