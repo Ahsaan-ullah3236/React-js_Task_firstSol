7f2e97d600ae5005912e25e642fb643f8f7d7670
Ahsaan-ullah3236 <158459990+Ahsaan-ullah3236@users.noreply.github.com>
2025-04-10 17:37:11 +0500

    feat: add ClientsLine component to display client satisfaction metrics

---
+++ my-first-nextjs-app/src/app/clientsline/page.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function clientsline() {
+  return (
+    <>
+       <div className="w-full mt-12 py-10 px-6 bg-[#002f46] overflow-hidden">
+  <div className="flex justify-center flex-wrap gap-6">
+    <div className="text-center px-32 py-2">
+      <span className="text-[#00D280]  font-bold text-3xl block mb-1 py-4">100 %</span>
+      <p className="text-white text-bold font-bold">Clients Satisfaction</p>
+    </div>
+    <div className="text-center px-32 py-2">
+      <span className="text-[#00D280] font-bold text-3xl block mb-1 py-4">80 +</span>
+      <p className="text-white text-bold font-bold">Happy Clients</p>
+    </div>
+    <div className="text-center px-32 py-2">
+      <span className="text-[#00D280] font-bold text-3xl block mb-1 py-4">100 %</span>
+      <p className="text-white text-bold font-bold">On Time Delivery</p>
+    </div>
+    <div className="text-center px-32 py-2">
+      <span className="text-[#00D280] font-bold text-3xl block mb-1 py-4">65</span>
+      <p className="text-white text-bold font-bold">Projects Delivered</p>
+    </div>
+  </div>
+</div>
+
+    </>
+  )
+}
+
+export default clientsline;
