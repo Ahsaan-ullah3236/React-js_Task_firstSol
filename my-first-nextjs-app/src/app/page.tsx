19df09794ee6dc67d5e23f447bb4b8b32a1ba985
Ahsaan-ullah3236 <158459990+Ahsaan-ullah3236@users.noreply.github.com>
2025-04-11 19:25:41 +0500

    fix: uncomment Innovation, Success, and Question components in Home page for proper rendering

--- my-first-nextjs-app/src/app/page.tsx
+++ my-first-nextjs-app/src/app/page.tsx
@@ -2,9 +2,12 @@ import Navbar from "./components/Navbar";
 import Herosection from "./Herosection/page";
 import Ourservices from "./Ourservices/page";
 import ClientsLine from "./clientsline/page";
-import Topchoice from "./Topchoice/page"
-import Ourprocess from "./Ourprocess/page"
-// import Innovation from "./Innovation/page"
+import Topchoice from "./Topchoice/page";
+import Ourprocess from "./Ourprocess/page";
+import Innovation from "./Innovation/page";
+import Success from "./Success/page";
+import Question from "./Question/page"
+
 
 
 
@@ -17,8 +20,9 @@ export default function Home() {
     <ClientsLine/>
     <Topchoice/>
     <Ourprocess/>
-    {/* <Innovation/> */}
-    
+    <Innovation/>
+    <Success/>
+    <Question/>
    </>
   );
 }
